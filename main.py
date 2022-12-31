e3b39de761b3b066f566131382ffceb3290e9a4f
DaaRiya <58295894+DaaRiya@users.noreply.github.com>
2022-12-31 11:32:18 +0300

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+def Factorial(n):
+    isGood = str(n).isnumeric()
+    if not isGood:  # if incorrect input
+        return None
+    else:  # else going to calculate factorial
+        result = 1
+        for i in range(2, int(n)+1):
+            result *= i
+        return result
+
+
+if __name__ == '__main__':
+    print('Waiting for input n...')
+    n = 0.5
+    print(f'for n ={n}, factorial = {Factorial_error(n)}')
